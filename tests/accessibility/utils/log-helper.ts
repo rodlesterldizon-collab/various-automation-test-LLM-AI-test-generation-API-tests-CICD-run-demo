394f47ceb7a1e971394e9be2003673b88bcd87a0
dizonrl20 <dizonrl20@gmail.com>
2026-08-14 18:03:37 -0400

    update: updated  the playwright config and playwright yml

--- tests/accessibility/utils/log-helper.ts
+++ tests/accessibility/utils/log-helper.ts
@@ -12,6 +12,10 @@ export function logViolationsAsWarning(url: string, violations: Result[]) {
     console.warn(chalk.white(`Description: ${violation.help}`));
     console.warn(chalk.cyan(`Help: ${violation.helpUrl}`));
     
+    if (process.env.CI) {
+      console.log(`::warning title=Accessibility Violation [${impact}]::${violation.id} on ${url}: ${violation.help}`);
+    }
+
     if (violation.nodes && violation.nodes.length > 0) {
       console.warn(chalk.gray(`Affected Nodes:`));
       for (const node of violation.nodes) {
